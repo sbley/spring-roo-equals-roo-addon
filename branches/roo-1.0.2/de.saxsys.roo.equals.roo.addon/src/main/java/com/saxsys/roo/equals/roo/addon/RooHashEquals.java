38c101bc84c5c6ca3274566a1c94309d070a3ff1
stefan.bley@saxsys.de <stefan.bley@saxsys.de@8a679fd9-62e5-6492-7910-c03aef8e19a2>
2010-04-07 14:57:30 +0000

    Branching to use with Roo 1.0.2 (non-OSGi)

---
+++ branches/roo-1.0.2/de.saxsys.roo.equals.roo.addon/src/main/java/com/saxsys/roo/equals/roo/addon/RooHashEquals.java
@@ -0,0 +1,18 @@
+package com.saxsys.roo.equals.roo.addon;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Provides {@link Object#equals(Object)} and {@link Object#hashCode()} methods if
+ * requested.
+ * 
+ * @author stefan.bley
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface RooHashEquals {
+
+}
