b8a51c509db3935dad414360a22ca1c3384e4f18
stefan.bley@saxsys.de <stefan.bley@saxsys.de@8a679fd9-62e5-6492-7910-c03aef8e19a2>
2010-04-08 09:27:06 +0000

    annotation attributes callSuper and callInstanceof implemented

--- trunk/de.saxsys.roo.equals.roo.addon/src/main/java/com/saxsys/roo/equals/roo/addon/RooHashEquals.java
+++ trunk/de.saxsys.roo.equals.roo.addon/src/main/java/com/saxsys/roo/equals/roo/addon/RooHashEquals.java
@@ -6,13 +6,28 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Provides {@link Object#equals(Object)} and {@link Object#hashCode()} methods if
- * requested.
+ * Provides {@link Object#equals(Object)} and {@link Object#hashCode()} methods
+ * if requested.
  * 
  * @author stefan.bley
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
 public @interface RooHashEquals {
+	/**
+	 * If {@code true} (default) then inserts calls to the super class equals()
+	 * and hashCode() methods.
+	 */
+	boolean callSuper() default true;
 
+	/**
+	 * <p>
+	 * If {@code true} then performs class comparison in equals() method using
+	 * {@code instanceof} rather than {@code getClass()}.
+	 * </p>
+	 * <p>
+	 * Defaults to {@code false}.
+	 * </p>
+	 */
+	boolean callInstanceof() default false;
 }
