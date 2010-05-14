27d79ecdd4d4d18a79479123ba79e420122a0fea
stefan.ocke <stefan.ocke@8a679fd9-62e5-6492-7910-c03aef8e19a2>
2010-05-14 14:05:25 +0000

    some cleanup

--- trunk/de.saxsys.roo.equals.addon/src/main/java/de/saxsys/roo/equals/addon/EqualsMetadataProvider.java
+++ trunk/de.saxsys.roo.equals.addon/src/main/java/de/saxsys/roo/equals/addon/EqualsMetadataProvider.java
@@ -3,7 +3,6 @@ package de.saxsys.roo.equals.addon;
 import java.util.logging.Logger;
 
 import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
 import org.osgi.service.component.ComponentContext;
 import org.springframework.roo.classpath.PhysicalTypeIdentifier;
@@ -11,9 +10,7 @@ import org.springframework.roo.classpath.PhysicalTypeMetadata;
 import org.springframework.roo.classpath.itd.AbstractItdMetadataProvider;
 import org.springframework.roo.classpath.itd.ItdTypeDetailsProvidingMetadataItem;
 import org.springframework.roo.model.JavaType;
-import org.springframework.roo.project.Dependency;
 import org.springframework.roo.project.Path;
-import org.springframework.roo.project.ProjectOperations;
 import org.springframework.roo.support.logging.HandlerUtils;
 
 /**
@@ -25,9 +22,6 @@ public final class EqualsMetadataProvider extends AbstractItdMetadataProvider {
 
 	protected final Logger logger = HandlerUtils.getLogger(getClass());
 
-	@Reference
-	private ProjectOperations projectOperations;
-
 	protected void activate(ComponentContext context) {
 		metadataDependencyRegistry.registerDependency(PhysicalTypeIdentifier
 				.getMetadataIdentiferType(), getProvidesType());
@@ -38,8 +32,6 @@ public final class EqualsMetadataProvider extends AbstractItdMetadataProvider {
 			String metadataIdentificationString, JavaType aspectName,
 			PhysicalTypeMetadata governorPhysicalTypeMetadata,
 			String itdFilename) {
-
-		addCommonsLangToClasspath(projectOperations);
 
 		// Create the metadata
 		return new EqualsMetadata(metadataIdentificationString, aspectName,
@@ -66,12 +58,5 @@ public final class EqualsMetadataProvider extends AbstractItdMetadataProvider {
 
 	public String getProvidesType() {
 		return EqualsMetadata.getMetadataIdentiferType();
-	}
-
-	protected void addCommonsLangToClasspath(
-			final ProjectOperations projectOperations) {
-//		Dependency dependency = new Dependency("commons-lang", "commons-lang",
-//				"2.5");
-//		projectOperations.dependencyUpdate(dependency);
 	}
 }
