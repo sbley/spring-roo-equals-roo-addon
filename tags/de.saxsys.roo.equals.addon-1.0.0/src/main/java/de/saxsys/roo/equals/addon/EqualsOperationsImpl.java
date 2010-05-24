7275029625c7a1e864e3373052e9c8635995dbb5
stefan.ocke <stefan.ocke@8a679fd9-62e5-6492-7910-c03aef8e19a2>
2010-05-24 12:19:58 +0000

    [maven-release-plugin]  copy for tag de.saxsys.roo.equals.addon-1.0.0

---
+++ tags/de.saxsys.roo.equals.addon-1.0.0/src/main/java/de/saxsys/roo/equals/addon/EqualsOperationsImpl.java
@@ -0,0 +1,186 @@
+package de.saxsys.roo.equals.addon;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.springframework.roo.classpath.PhysicalTypeDetails;
+import org.springframework.roo.classpath.PhysicalTypeMetadata;
+import org.springframework.roo.classpath.PhysicalTypeMetadataProvider;
+import org.springframework.roo.classpath.details.MemberFindingUtils;
+import org.springframework.roo.classpath.details.MutableClassOrInterfaceTypeDetails;
+import org.springframework.roo.classpath.details.annotations.AnnotationAttributeValue;
+import org.springframework.roo.classpath.details.annotations.AnnotationMetadata;
+import org.springframework.roo.classpath.details.annotations.BooleanAttributeValue;
+import org.springframework.roo.classpath.details.annotations.DefaultAnnotationMetadata;
+import org.springframework.roo.metadata.MetadataService;
+import org.springframework.roo.model.JavaSymbolName;
+import org.springframework.roo.model.JavaType;
+import org.springframework.roo.project.Dependency;
+import org.springframework.roo.project.ProjectMetadata;
+import org.springframework.roo.project.ProjectOperations;
+import org.springframework.roo.project.Repository;
+import org.springframework.roo.support.logging.HandlerUtils;
+import org.springframework.roo.support.util.Assert;
+
+/**
+ * Implementation of commands that are available via the Roo shell.
+ * 
+ * @author stefan.bley
+ * @author stefan.ocke
+ * @since 1.0.0
+ */
+@Component
+@Service
+public class EqualsOperationsImpl implements EqualsOperations {
+
+	private static final JavaSymbolName CALLSUPER = new JavaSymbolName(
+			"callSuper");
+
+	private static final JavaSymbolName CALLINSTANCEOF = new JavaSymbolName(
+			"callInstanceof");
+
+	private final Logger logger = HandlerUtils.getLogger(getClass());
+
+	@Reference
+	private MetadataService metadataService;
+
+	@Reference
+	private ProjectOperations projectOperations;
+
+	@Reference
+	private PhysicalTypeMetadataProvider physicalTypeMetadataProvider;
+
+	public boolean isProjectAvailable() {
+		return getProjectMetadata() != null;
+	}
+
+	/**
+	 * @return the project metadata or null if there is no user project
+	 */
+	ProjectMetadata getProjectMetadata() {
+		ProjectMetadata projectMetadata = (ProjectMetadata) metadataService
+				.get(ProjectMetadata.getProjectIdentifier());
+		return projectMetadata;
+	}
+
+	public void addEquals(final JavaType typeName, final Boolean callSuper,
+			final Boolean callInstanceOf) {
+		addDependencies();
+		addEqualsAnnotation(typeName, callSuper, callInstanceOf);
+	}
+
+	/**
+	 * Adds the dependency to the annotation jar and to required libraries to
+	 * the project's pom.xml.
+	 * <p>
+	 * Additionally, it inserts the repository which hosts the annotation jar
+	 * into the pom.
+	 * </p>
+	 */
+	private void addDependencies() {
+		String repoId = "";
+		String repoName = "Repository for Roo equals addon";
+		String repoUrl = "";
+		String annotationsGroupId = "";
+		String annotationsArtifactId = "";
+		String annotationsVersion = "";
+		InputStream inputStream = getClass().getClassLoader()
+				.getResourceAsStream("dependencies.properties");
+		try {
+			Properties properties = new Properties();
+			properties.load(inputStream);
+			annotationsGroupId = properties.getProperty("annotations.groupId");
+			annotationsArtifactId = properties
+					.getProperty("annotations.artifactId");
+			annotationsVersion = properties.getProperty("annotations.version");
+			boolean isSnapshot = annotationsVersion.endsWith("SNAPSHOT");
+			if (isSnapshot) {
+				repoId = properties.getProperty("annotations.snapshotrepo.id");
+				repoUrl = properties
+						.getProperty("annotations.snapshotrepo.url");
+			} else {
+				repoId = properties.getProperty("annotations.repo.id");
+				repoUrl = properties.getProperty("annotations.repo.url");
+			}
+			String commonslangVersion = properties
+					.getProperty("commonslang.version");
+
+			projectOperations.addRepository(new Repository(repoId, repoName, repoUrl, isSnapshot));
+
+			Dependency dependencyAnno = new Dependency(annotationsGroupId,
+					annotationsArtifactId, annotationsVersion);
+			projectOperations.dependencyUpdate(dependencyAnno);
+
+			Dependency dependencyCmnLang = new Dependency("commons-lang",
+					"commons-lang", commonslangVersion);
+			projectOperations.dependencyUpdate(dependencyCmnLang);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * Adds the annotation {@link RooEquals} to the class declaration.
+	 * 
+	 * @param typeName
+	 *            a class
+	 * @param callInstanceOf
+	 * @param callSuper
+	 */
+	private void addEqualsAnnotation(final JavaType typeName,
+			Boolean callSuper, Boolean callInstanceOf) {
+		String id = physicalTypeMetadataProvider.findIdentifier(typeName);
+		if (id == null) {
+			logger.warning("Cannot locate source for '"
+					+ typeName.getFullyQualifiedTypeName() + "'");
+			return;
+		}
+
+		PhysicalTypeMetadata physicalTypeMetadata = (PhysicalTypeMetadata) metadataService
+				.get(id);
+		if (physicalTypeMetadata == null) {
+			// For some reason we found the source file a few lines ago, but
+			// suddenly it has gone away
+			logger
+					.warning("Cannot provide equals because '"
+							+ typeName.getFullyQualifiedTypeName()
+							+ "' is unavailable");
+			return;
+		}
+
+		PhysicalTypeDetails ptd = physicalTypeMetadata.getPhysicalTypeDetails();
+		Assert.isInstanceOf(MutableClassOrInterfaceTypeDetails.class, ptd);
+		MutableClassOrInterfaceTypeDetails mutable = (MutableClassOrInterfaceTypeDetails) ptd;
+
+		JavaType annotationType = new JavaType(RooEquals.class.getName());
+		// remove the old annotation
+		List<? extends AnnotationMetadata> annotations = mutable
+				.getTypeAnnotations();
+
+		AnnotationMetadata found = MemberFindingUtils.getAnnotationOfType(
+				annotations, annotationType);
+		if (found != null) {
+			mutable.removeTypeAnnotation(annotationType);
+		}
+
+		// add the new annotation
+		List<AnnotationAttributeValue<?>> attributes = new ArrayList<AnnotationAttributeValue<?>>();
+		if (callSuper != null) {
+
+			attributes.add(new BooleanAttributeValue(CALLSUPER, callSuper));
+		}
+		if (callInstanceOf != null) {
+
+			attributes.add(new BooleanAttributeValue(CALLINSTANCEOF, callInstanceOf));
+		}
+		AnnotationMetadata annotation = new DefaultAnnotationMetadata(
+				annotationType, attributes);
+		mutable.addTypeAnnotation(annotation);
+	}
+}
