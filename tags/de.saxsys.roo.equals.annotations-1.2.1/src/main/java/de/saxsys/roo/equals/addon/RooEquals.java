6fdc90dabbe3a0d6de2951a19089bf5f308bac93
stefan.bley@saxsys.de <stefan.bley@saxsys.de@8a679fd9-62e5-6492-7910-c03aef8e19a2>
2011-02-12 15:35:11 +0000

    [maven-release-plugin]  copy for tag de.saxsys.roo.equals.annotations-1.2.1

---
+++ tags/de.saxsys.roo.equals.annotations-1.2.1/src/main/java/de/saxsys/roo/equals/addon/RooEquals.java
@@ -0,0 +1,45 @@
+package de.saxsys.roo.equals.addon;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Provides {@link Object#equals(Object)} and {@link Object#hashCode()} methods
+ * if requested.
+ * 
+ * @author stefan.bley
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface RooEquals {
+	/**
+	 * If {@code true} then inserts calls to the super class equals()
+	 * and hashCode() methods.
+	 * <p>
+	 * Defaults to {@code false}.
+	 * </p>
+	 */
+	boolean callSuper() default false;
+
+	/**
+	 * <p>
+	 * If {@code true} then performs class comparison in equals() method using
+	 * {@code instanceof} rather than {@code getClass()}.
+	 * </p>
+	 * <p>
+	 * Defaults to {@code false}.
+	 * </p>
+	 */
+	boolean callInstanceof() default false;
+	
+	/**
+	 * If set to {@code true}, exclude all fields except the ones annotated with {@link EqualsInclude}.
+	 * If set to {@code false}, include all fields except for ones annotated with {@link EqualsExclude}.
+	 * <p>
+	 * Defaults to {@code false}.
+	 * </p> 
+	 */
+	boolean exclusiveMode() default false;
+}
