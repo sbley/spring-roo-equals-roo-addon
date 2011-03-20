e035dd2cf8b9f81e4909a1c600095683ca81fcf1
stefan.bley@saxsys.de <stefan.bley@saxsys.de@8a679fd9-62e5-6492-7910-c03aef8e19a2>
2011-03-20 15:53:29 +0000

    [maven-release-plugin]  copy for tag de.saxsys.roo.equals.addon-1.3.2

---
+++ tags/de.saxsys.roo.equals.addon-1.3.2/src/main/java/de/saxsys/roo/equals/addon/EqualsOperations.java
@@ -0,0 +1,27 @@
+package de.saxsys.roo.equals.addon;
+
+import org.springframework.roo.model.JavaType;
+
+/**
+ * Interface to {@link EqualsOperationsImpl}.
+ * 
+ * @author stefan.bley
+ * @since 1.0.0
+ */
+public interface EqualsOperations {
+
+	/**
+	 * Returns if a project is available in the current Roo Shell.
+	 */
+	boolean isProjectAvailable();
+
+	/**
+	 * Provides equals() and hashCode() method for the given class.
+	 * 
+	 * @param typeName
+	 *            a class
+	 * @param callInstanceOf 
+	 * @param callSuper 
+	 */
+	void addEquals(JavaType typeName, Boolean callSuper, Boolean callInstanceOf, Boolean exclusiveMode);
+}
